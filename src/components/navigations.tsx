4b953bf19d150372be90b5ec916543eaad798241
officer-kd6-3dot7 <punithg360@gmail.com>
2026-07-15 13:40:19 +0530

    nav button update

--- src/components/navigations.tsx
+++ src/components/navigations.tsx
@@ -14,23 +14,26 @@ export function Navigations() {
   const prev = flatMenuItems[index - 1];
   const next = flatMenuItems[index + 1];
   return (
-    <div className="sticky bottom-0 flex h-16 w-full items-center justify-stretch p-0">
+    <div className="sticky bottom-0 flex h-20 w-full items-center justify-stretch p-0">
       {prev && (
         <NavButton
           href={prev.url}
-          className="absolute left-[-15.5] origin-left scale-90 md:left-0"
+          className="absolute left-[-15.5] origin-left scale-85 md:left-0"
+          bClassName="items-end py-4"
           leftIcon={<ChevronLeft />}
         >
+          <span className="text-xs text-gray-400">Previous</span>
           {prev.title}
         </NavButton>
       )}
       {next && (
         <NavButton
           href={next.url}
-          className="absolute right-[-15.5] origin-right scale-90 md:right-0"
-          bClassName="text-transparent bg-gradient-to-r from-emerald-400 to-lime-400"
+          className="absolute right-[-15.5] origin-right scale-85 md:right-0"
+          bClassName="text-transparent bg-gradient-to-r from-emerald-400 to-lime-400 items-start"
           rightIcon={<ChevronRight className="text-lime-500" />}
         >
+          <span className="text-xs text-gray-400">Next</span>
           {next.title}
         </NavButton>
       )}
@@ -45,23 +48,26 @@ function NavButton({
   bClassName,
   leftIcon,
   rightIcon,
-}: PropsWithChildren<{
-  bClassName?: string;
-  className?: string;
-  href: string;
-  leftIcon?: ReactNode;
-  rightIcon?: ReactNode;
-}>) {
+}: PropsWithChildren<
+  {
+    bClassName?: string;
+    className?: string;
+    href: string;
+  } & (
+    | { leftIcon?: undefined; rightIcon?: ReactNode }
+    | { leftIcon?: ReactNode; rightIcon?: undefined }
+  )
+>) {
   return (
     <Link href={href} className={cn(className)}>
       <Button
         variant="outline"
-        className="dark:bg-sidebar hover:dark:bg-sidebar m-0! md:scale-100"
+        className="dark:bg-sidebar hover:dark:bg-sidebar m-0! py-6! md:scale-100"
       >
         {leftIcon}
         <span
           className={cn(
-            "flex items-center justify-center bg-linear-to-r bg-clip-text",
+            "flex flex-col bg-linear-to-r bg-clip-text",
             bClassName,
           )}
         >
